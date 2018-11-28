7be057b69453b5cdae39a70966e94181877d5ce7
Stef Graces <stefgraces@Stefs-MacBook-Pro.local>
2018-11-28 11:32:37 +0100

    added method to images

--- ovc/images.go
+++ ovc/images.go
@@ -24,6 +24,7 @@ type ImageConfig struct {
 type ImageService interface {
 	Upload(*ImageConfig) error
 	DeleteByID(int) error
+	DeleteSystemImageByID(int, string) error
 }
 
 // ImageServiceOp handles communication with the image related methods of the
@@ -61,6 +62,27 @@ func (s *ImageServiceOp) DeleteByID(imageID int) error {
 		return err
 	}
 	req, err := http.NewRequest("POST", s.client.ServerURL+"/cloudapi/images/delete", bytes.NewBuffer(imageJSON))
+	if err != nil {
+		return err
+	}
+	_, err = s.client.Do(req)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// DeleteSystemImageByID deletes an existing system image by ID
+func (s *ImageServiceOp) DeleteSystemImageByID(imageID int, reason string) error {
+	imageMap := make(map[string]interface{})
+	imageMap["imageId"] = imageID
+	imageMap["reason"] = reason
+	imageMap["permanently"] = true
+	imageJSON, err := json.Marshal(imageMap)
+	if err != nil {
+		return err
+	}
+	req, err := http.NewRequest("POST", s.client.ServerURL+"/cloudbroker/image/delete", bytes.NewBuffer(imageJSON))
 	if err != nil {
 		return err
 	}
