5a035a0d5cc41402e64f35212342883d52963920
Chris Van de Gejuchte <chris.vandegejuchte@gmail.com>
2019-06-19 14:25:15 +0200

    Adds diskinfo pci bus

--- ovc/disks.go
+++ ovc/disks.go
@@ -45,6 +45,7 @@ type DiskInfo struct {
 	Images              []interface{} `json:"images"`
 	GUID                int           `json:"guid"`
 	ID                  int           `json:"id"`
+	PCIBus              string        `json:"pci_bus"`
 	PCISlot             string        `json:"pci_slot"`
 	AccountID           int           `json:"accountId"`
 	SizeUsed            int           `json:"sizeUsed"`
