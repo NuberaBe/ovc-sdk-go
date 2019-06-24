b080e086196d5c2740b58851a87aadf8f0e27dd6
Chris Van de Gejuchte <chris.vandegejuchte@gmail.com>
2019-06-24 11:27:19 +0200

    Small refactor (#17)
    - Renames gid to gridID
    - Consistent capitalisation of ID suffix

--- ovc/disks.go
+++ ovc/disks.go
@@ -11,7 +11,7 @@ import (
 // DiskConfig is used when creating a disk
 type DiskConfig struct {
 	AccountID   int    `json:"accountId,omitempty"`
-	GID         int    `json:"gid,omitempty"`
+	GridID      int    `json:"gid,omitempty"`
 	MachineID   int    `json:"machineId,omitempty"`
 	DiskName    string `json:"diskName,omitempty"`
 	Name        string `json:"name,omitempty"`
@@ -50,7 +50,7 @@ type DiskInfo struct {
 	AccountID           int           `json:"accountId"`
 	SizeUsed            int           `json:"sizeUsed"`
 	Descr               string        `json:"descr"`
-	Gid                 int           `json:"gid"`
+	GridID              int           `json:"gid"`
 	Role                string        `json:"role"`
 	Params              string        `json:"params"`
 	Type                string        `json:"type"`
@@ -288,7 +288,7 @@ func (s *DiskServiceOp) Get(diskID string) (*DiskInfo, error) {
 	return diskInfo, nil
 }
 
-// GetByName gets a disk by its maxsize
+// GetByName gets a disk by its name
 func (s *DiskServiceOp) GetByName(name string, accountID string) (*DiskInfo, error) {
 	aid, err := strconv.Atoi(accountID)
 	if err != nil {
@@ -305,7 +305,7 @@ func (s *DiskServiceOp) GetByName(name string, accountID string) (*DiskInfo, err
 		}
 	}
 
-	return nil, errors.New("Could not find disk based on maxsize")
+	return nil, errors.New("Could not find disk based on name")
 }
 
 // Resize resizes a disk. Can only increase the size of a disk
